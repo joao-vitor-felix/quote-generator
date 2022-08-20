7ca29042cbf230be57a5d87c77b786bfe93f88ef
joao-vitor <joaovitorfelixcontato@gmail.com>
2022-08-20 11:27:10 -0300

    Finalizado projeto

---
+++ script.js
@@ -0,0 +1,67 @@
+// Variables
+const quoteContainer = document.getElementById("quote-container");
+const quote = document.getElementById("content");
+const author = document.getElementById("author");
+const twitter = document.getElementById("twitter");
+const newQuote = document.getElementById("newQuote");
+const loader = document.getElementById("loader");
+let apiQuotes = [];
+
+// Show Laoding
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide Loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+// Twitter Integration
+function twitterButton() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+function quotes() {
+  loading();
+  // Check if Author field is blank and replace it
+  if (!apiQuotes.author) {
+    author.textContent = "Unknown";
+  } else {
+    author.textContent = apiQuotes.author;
+  }
+
+  // Check Quote lenght to style
+  if (apiQuotes.content.lenght > 5) {
+    quote.classList.add("long-quote");
+    author.classList.add("long-quote-author");
+  } else {
+    quote.classList.remove("long-quote");
+    author.classList.remove("long-quote-author");
+  }
+  quote.textContent = apiQuotes.content;
+  complete();
+}
+
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://api.quotable.io/random";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    quotes();
+    return apiQuotes;
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Event Listeners
+newQuote.addEventListener("click", getQuotes);
+twitter.addEventListener("click", twitterButton);
+
+// On load
+getQuotes();
